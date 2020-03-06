fa3b1bd1aad01ae4fd502901bb4c518a314343a5
Riadh <r.fezzani@vitadx.com>
2020-03-06 18:25:10 +0100

    Define signed integrate function

--- skimage/_shared/transform.pxd
+++ skimage/_shared/transform.pxd
@@ -1,6 +1,8 @@
-from .fused_numerics cimport np_anyint, np_floats
+import numpy as np
+cimport numpy as cnp
+from .fused_numerics cimport np_signed_numeric, np_real_numeric, np_uints
 
 
-cdef np_signed_numeric integrate(np_real_numeric[:, ::1] sat,
+cpdef np_signed_numeric integrate(np_signed_numeric[:, ::1] sat,
                                  Py_ssize_t r0, Py_ssize_t c0,
                                  Py_ssize_t r1, Py_ssize_t c1) nogil
--- skimage/_shared/transform.pyx
+++ skimage/_shared/transform.pyx
@@ -2,11 +2,38 @@
 #cython: boundscheck=False
 #cython: nonecheck=False
 #cython: wraparound=False
-import numpy as np
-cimport numpy as cnp
 
 
-cdef cnp.int64_tint integrate(np_anyint[:, ::1] sat,
+cpdef np_signed_numeric integrate(np_signed_numeric[:, ::1] sat,
+                                  Py_ssize_t r0, Py_ssize_t c0,
+                                  Py_ssize_t r1, Py_ssize_t c1) nogil:
+    """
+    Using a summed area table / integral image, calculate the sum
+    over a given window.
+
+    This function is the same as the `integrate` function in
+    `skimage.transform.integrate`, but this Cython version significantly
+    speeds up the code.
+
+    Parameters
+    ----------
+    sat : ndarray of np_real_numeric
+        Summed area table / integral image.
+    r0, c0 : Py_ssize_t
+        Top-left corner of block to be summed.
+    r1, c1 : Py_ssize_t
+        Bottom-right corner of block to be summed.
+
+    Returns
+    -------
+    S : np_signed_numeric
+        Sum over the given window.
+    """
+
+    return  _integrate(sat, r0, c0, r1, c1)
+
+
+cdef cnp.int64_t _integrate_u(np_uints[:, ::1] sat,
                               Py_ssize_t r0, Py_ssize_t c0,
                               Py_ssize_t r1, Py_ssize_t c1) nogil:
     """
@@ -32,7 +59,7 @@ cdef cnp.int64_tint integrate(np_anyint[:, ::1] sat,
         Sum over the given window.
     """
 
-    cdef int S = 0
+    cdef cnp.int64_t S = 0
 
     S += sat[r1, c1]
 
@@ -48,9 +75,9 @@ cdef cnp.int64_tint integrate(np_anyint[:, ::1] sat,
     return S
 
 
-cdef np_floats integrate(np_floats[:, ::1] sat,
-                         Py_ssize_t r0, Py_ssize_t c0,
-                         Py_ssize_t r1, Py_ssize_t c1) nogil:
+cdef np_signed_numeric _integrate(np_signed_numeric[:, ::1] sat,
+                                  Py_ssize_t r0, Py_ssize_t c0,
+                                  Py_ssize_t r1, Py_ssize_t c1) nogil:
     """
     Using a summed area table / integral image, calculate the sum
     over a given window.
@@ -74,7 +101,7 @@ cdef np_floats integrate(np_floats[:, ::1] sat,
         Sum over the given window.
     """
 
-    cdef np_floats S = 0
+    cdef np_signed_numeric S = 0
 
     S += sat[r1, c1]
 
