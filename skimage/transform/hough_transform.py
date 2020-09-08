31b1dcb968028597e412a52b127507a299823f88
Riadh <r.fezzani@vitadx.com>
2020-09-08 17:10:14 +0200

    Move _prominent_peaks from peaks to hough_transform

--- skimage/transform/hough_transform.py
+++ skimage/transform/hough_transform.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy.spatial import cKDTree
+import scipy.ndimage as ndi
 from ._hough_transform import (_hough_circle,
                                _hough_ellipse,
                                _hough_line,
                                _probabilistic_hough_line as _prob_hough_line)
+from ..measure import label, regionprops
 
 
 def hough_line_peaks(hspace, angles, dists, min_distance=9, min_angle=10,
@@ -56,8 +58,6 @@ def hough_line_peaks(hspace, angles, dists, min_distance=9, min_angle=10,
     2
 
     """
-    from ..feature.peak import _prominent_peaks
-
     h, a, d = _prominent_peaks(hspace, min_xdistance=min_angle,
                                min_ydistance=min_distance,
                                threshold=threshold,
@@ -325,7 +325,6 @@ def hough_circle_peaks(hspaces, radii, min_xdistance=1, min_ydistance=1,
     Otherwise, circles will be returned in the order of decreasing voting
     number.
     """
-    from ..feature.peak import _prominent_peaks
 
     r = []
     cx = []
@@ -425,3 +424,105 @@ def label_distant_points(xs, ys, min_xdistance, min_ydistance, max_points):
             n_pts += 1
     should_keep = ~is_neighbor
     return should_keep
+
+
+def _prominent_peaks(image, min_xdistance=1, min_ydistance=1,
+                     threshold=None, num_peaks=np.inf):
+    """Return peaks with non-maximum suppression.
+
+    Identifies most prominent features separated by certain distances.
+    Non-maximum suppression with different sizes is applied separately
+    in the first and second dimension of the image to identify peaks.
+
+    Parameters
+    ----------
+    image : (M, N) ndarray
+        Input image.
+    min_xdistance : int
+        Minimum distance separating features in the x dimension.
+    min_ydistance : int
+        Minimum distance separating features in the y dimension.
+    threshold : float
+        Minimum intensity of peaks. Default is `0.5 * max(image)`.
+    num_peaks : int
+        Maximum number of peaks. When the number of peaks exceeds `num_peaks`,
+        return `num_peaks` coordinates based on peak intensity.
+
+    Returns
+    -------
+    intensity, xcoords, ycoords : tuple of array
+        Peak intensity values, x and y indices.
+    """
+
+    img = image.copy()
+    rows, cols = img.shape
+
+    if threshold is None:
+        threshold = 0.5 * np.max(img)
+
+    ycoords_size = 2 * min_ydistance + 1
+    xcoords_size = 2 * min_xdistance + 1
+    img_max = ndi.maximum_filter1d(img, size=ycoords_size, axis=0,
+                                   mode='constant', cval=0)
+    ndi.maximum_filter1d(img_max, size=xcoords_size, axis=1,
+                         mode='constant', cval=0, output=img_max)
+    img *= (img == img_max)
+
+    label_img = label(img > threshold)
+    props = regionprops(label_img, img_max)
+
+    # Sort the list of peaks by intensity, not left-right, so larger peaks
+    # in Hough space cannot be arbitrarily suppressed by smaller neighbors
+    props = sorted(props, key=lambda x: x.max_intensity)[::-1]
+    coords = np.round([p.centroid for p in props]).astype(int)
+
+    img_peaks = []
+    ycoords_peaks = []
+    xcoords_peaks = []
+
+    # relative coordinate grid for local neighbourhood suppression
+    ycoords_ext, xcoords_ext = np.mgrid[-min_ydistance:min_ydistance + 1,
+                                        -min_xdistance:min_xdistance + 1]
+
+    for ycoords_idx, xcoords_idx in coords:
+        accum = img_max[ycoords_idx, xcoords_idx]
+        if accum > threshold:
+            # add current feature to peaks
+            img_peaks.append(accum)
+            ycoords_peaks.append(ycoords_idx)
+            xcoords_peaks.append(xcoords_idx)
+
+            # suppress neighbourhood
+            # absolute coordinate grid for local neighbourhood suppression
+            ycoords_nh = ycoords_idx + ycoords_ext
+            xcoords_nh = xcoords_idx + xcoords_ext
+
+            # no reflection for distance neighbourhood
+            ycoords_in = np.logical_and(ycoords_nh >= 0, ycoords_nh < rows)
+            ycoords_nh = ycoords_nh[ycoords_in]
+            xcoords_nh = xcoords_nh[ycoords_in]
+
+            # reflect xcoords and assume xcoords are continuous,
+            # e.g. for angles:
+            # (..., 88, 89, -90, -89, ..., 89, -90, -89, ...)
+            xcoords_low = xcoords_nh < 0
+            ycoords_nh[xcoords_low] = rows - ycoords_nh[xcoords_low]
+            xcoords_nh[xcoords_low] += cols
+
+            xcoords_high = xcoords_nh >= cols
+            ycoords_nh[xcoords_high] = rows - ycoords_nh[xcoords_high]
+            xcoords_nh[xcoords_high] -= cols
+
+            img_max[ycoords_nh, xcoords_nh] = 0
+
+    img_peaks = np.array(img_peaks)
+    ycoords_peaks = np.array(ycoords_peaks)
+    xcoords_peaks = np.array(xcoords_peaks)
+
+    if num_peaks < len(img_peaks):
+        idx_maxsort = np.argsort(img_peaks)[::-1][:num_peaks]
+        img_peaks = img_peaks[idx_maxsort]
+        ycoords_peaks = ycoords_peaks[idx_maxsort]
+        xcoords_peaks = xcoords_peaks[idx_maxsort]
+
+    return img_peaks, xcoords_peaks, ycoords_peaks
